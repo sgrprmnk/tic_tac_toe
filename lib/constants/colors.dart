2927dccf164d9b36d5d68131e2f5a0eb36d961db
Sagar Pramanik <sgrprmnk@gmail.com>
2025-12-11 18:21:30 +0530

    Add files via upload

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MainColor{
+  static Color primaryColor=Color(0xFFff4b4b);
+  static const matchedColor = Colors.purpleAccent;
+  static Color secondaryColor=Color(0xFFffca27);
+  static Color accentColor=Color(0xFF4169e8);
+}
